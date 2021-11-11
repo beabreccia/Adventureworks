7881cef8b00cb196d7b4229d3bfa0b080273b488
amorcomrabiscos@gmail.com <amorcomrabiscos@gmail.com>
2021-11-11 13:47:16 +0000

    COMMIT

--- models/adventureworks/staging/stg_country_region.sql
+++
@@ -1,9 +0,0 @@
-with 
-  source_data as (
-    select 
-      countryregioncode
-      , name
-      , modifieddate
-    from {{ source('adventureworks_etl', 'countryregion')  }}
-  )
-select * from source_data
